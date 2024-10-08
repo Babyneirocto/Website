1cbf0482e06665bc5ea3afd758925c268eb61a2b
Babyneirocto <dev@babyneiro.me>
2024-10-08 16:45:49 +0800

    main

---
+++ src/screens/Frame/Frame.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="div">
+        <div className="overlap">
+          <div className="rectangle" />
+          <div className="rectangle-2" />
+          <div className="rectangle-3" />
+          <img className="image" alt="Image" src="/img/image-1.png" />
+          <div className="group">
+            <img className="img" alt="Image" src="/img/image-3.png" />
+            <img className="image-2" alt="Image" src="/img/image-6.png" />
+          </div>
+          <p className="BABY-NEIRO-salute-to">
+            BABY NEIRO: <br />
+            salute to the 2nd lovely member <br />
+            of the Doge Universe
+          </p>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group">
+              <div className="text-wrapper">W-O----O-F</div>
+              <div className="text-wrapper-2">W-O----O-F</div>
+              <div className="text-wrapper-3">W-O----O-F</div>
+            </div>
+          </div>
+          <p className="p">
+            BabyNeiro was created as a tribute to the late Doge meme dog, Kabosu. After Kabosu’s passing, a new Shiba
+            Inu named Neiro was adopted and quickly became the heart of the community with its unique charm and energy,
+            inspiring a group of crypto enthusiasts. We launched the BabyNeiro meme coin, incorporating Neiro’s image to
+            capture this new spirit. The coin quickly gained popularity within the community, not only continuing Doge’s
+            legacy of humor but also becoming a symbol of the next generation of meme culture. Baby Neiro has since
+            become a beloved icon, representing hope and joy for the future.
+          </p>
+          <img className="image-3" alt="Image" src="/img/image-7.png" />
+          <div className="image-wrapper">
+            <img className="image-4" alt="Image" src="/img/image-10.png" />
+          </div>
+          <img className="rectangle-4" alt="Rectangle" src="/img/rectangle-8.png" />
+          <div className="text-wrapper-4">CTO NOW!</div>
+          <div className="group-2">
+            <div className="text-wrapper-5">BABY NEIRO</div>
+            <div className="overlap-2">
+              <div className="ellipse" />
+              <img className="element" alt="Element" src="/img/1.png" />
+            </div>
+          </div>
+          <div className="text-wrapper-6">CA： 3W5XvarScGuGAMucEit1csjqj23Wow9YBGAoJUzyQaLQ</div>
+        </div>
+        <div className="text-wrapper-7">Tokenomics</div>
+        <div className="overlap-3">
+          <img className="image-5" alt="Image" src="/img/image-8.png" />
+          <div className="text-wrapper-8">Total Supply</div>
+          <div className="text-wrapper-9">829,639,493.68</div>
+        </div>
+        <div className="overlap-4">
+          <div className="text-wrapper-10">Current Supply</div>
+          <div className="text-wrapper-11">829,639,493.68</div>
+          <img
+            className="img-2"
+            alt="Telegram cloud photo"
+            src="/img/telegram-cloud-photo-size-5-6310002627018080751-y-1.png"
+          />
+        </div>
+        <div className="overlap-5">
+          <div className="text-wrapper-10">Liquidity</div>
+          <div className="text-wrapper-11">Burnt</div>
+          <img
+            className="img-2"
+            alt="Telegram cloud photo"
+            src="/img/telegram-cloud-photo-size-5-6310002627018080752-y-1.png"
+          />
+        </div>
+        <div className="overlap-6">
+          <div className="text-wrapper-10">Dev</div>
+          <div className="text-wrapper-11">Leave</div>
+          <img className="img-2" alt="Image" src="/img/image-9.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
